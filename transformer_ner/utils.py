ce76cff3b8159affda8bb119715292777235556e
Kao Pei-Wei <tif135@gmail.com>
2020-04-13 06:11:00 +0800

    transformer

--- transformer_ner/utils.py
+++ transformer_ner/utils.py
@@ -49,7 +49,7 @@ def make_dict(max_num, dict_path, train_path):
     word2id = dict()
     line_count = 0
     
-    for line in tqdm(open(train_path)):
+    for line in tqdm(open(train_path, encoding='utf-8')):
         line_count += 1.0
         for word in line.split():
             word_count[word] = word_count.get(word,0) + 1
@@ -143,7 +143,7 @@ class data_utils():
         for epo in range(num_epoch):
             start_time = time.time()
             print("start epo %d" % (epo))
-            for line1, line2 in zip(open(src_file), open(tgt_file)):
+            for line1, line2 in zip(open(src_file, encoding='utf-8'), open(tgt_file, encoding='utf-8')):
                 vec1 = self.text2id(line1.strip(), seq_length)
                 vec2 = self.labeltext2id(line2.strip(), seq_length)
 
