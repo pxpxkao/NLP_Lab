6b8ee91c16f910982931387d278574006c2ca479
Kao Pei-Wei <tif135@gmail.com>
2020-05-18 00:47:24 +0800

    pretrained

--- pretrained/run.sh
+++ pretrained/run.sh
@@ -5,15 +5,15 @@
 # curl -L 'https://sites.google.com/site/germeval2014ner/data/NER-de-test.tsv?attredirects=0&d=1' \
 # | grep -v "^#" | cut -f 2,3 | tr '\t' ' ' > test.txt.tmp
 # wget "https://raw.githubusercontent.com/stefan-it/fine-tuned-berts-seq/master/scripts/preprocess.py"
-export MAX_LENGTH=350
-export BERT_MODEL=roberta-base
+export MAX_LENGTH=300
+export BERT_MODEL=bert-base-cased
 # python3 preprocess.py train.txt.tmp $BERT_MODEL $MAX_LENGTH > train.txt
 # python3 preprocess.py dev.txt.tmp $BERT_MODEL $MAX_LENGTH > dev.txt
 # python3 preprocess.py test.txt.tmp $BERT_MODEL $MAX_LENGTH > test.txt
 # cat train.txt dev.txt test.txt | cut -d " " -f 2 | grep -v "^$"| sort | uniq > labels.txt
 export DATA_DIR=data
-export OUTPUT_DIR=roberta-base-model
-export BATCH_SIZE=8
+export OUTPUT_DIR=bert-base-model
+export BATCH_SIZE=32
 export NUM_EPOCHS=3
 export SAVE_STEPS=750
 export SEED=1
